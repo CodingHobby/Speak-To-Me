a5543ebd7d2cb9d53b088eba91040205d3e0ca0a
codinghobby@gmail.com <codinghobby@gmail.com>
2017-03-26 19:15:56 +0200

    Added support for different languages

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-$(function() {
+$(function () {
+	var curLang = navigator.languge || 'en-US'
+	console.log(curLang)
+
 	const input = $('#text')
-	$('#say').click(function(e) {
+
+	window.speechSynthesis.onvoiceschanged = function () {
+		$('#radios').html('')
+		var langs = speechSynthesis.getVoices().map(voice => voice.lang)
+		var prevLang
+		langs.forEach(lang => {
+			if (lang !== prevLang) {
+				if (lang != curLang) {
+					$('#radios').append('<li><input type="radio" name="radio" class="radio" value="' + lang + '"> ' + lang + '</li>')
+				} else {
+					$('#radios').append('<li><input type="radio" checked="checked" name="radio" class="radio" value="' + lang + '"> ' + lang + '</li>')
+				}
+				prevLang = lang
+			}
+			$('#radioForm input').on('change', function () {
+				curLang = $('input:checked', '#radioForm').val()
+			})
+		})
+
+	}
+
+	$('#say').click(function (e) {
 		e.preventDefault()
-		sayStuff(input.val())
+		sayStuff(input.val(), curLang)
 	})
-	$(document).keypress(function(e) {
-		if(e.which == 13) {
+
+	$(document).keypress(function (e) {
+		if (e.which == 13) {
 			e.preventDefault()
-			sayStuff(input.val())
+			sayStuff(input.val(), curLang)
 		}
 	})
 })
 
-function sayStuff(stuff) {
+
+function sayStuff(stuff, lang) {
 	const msg = new SpeechSynthesisUtterance(stuff)
+	msg.lang = lang
 	window.speechSynthesis.speak(msg)
 }
